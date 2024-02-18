91bd1025adf8dbe82657f64739621f53813eb751
sappysealsz <enchantercode@gmail.com>
2024-02-18 03:16:32 +0100

    Add util modules (isolated from Navbar component) - Module to store current network in localStorage - Module to manage the state of the UL element in Navbar component - Module to fetch latest block for the current network

---
+++ src/ts/utils/web3/navbarState.ts
@@ -0,0 +1,75 @@
+import { fetchLatestBlock } from "./fetchLatestBlock";
+import { storeNetwork } from "./storeNetwork";
+
+const connectEvent = (li: HTMLLIElement): void => {
+    li.addEventListener("click", async (): Promise<void> => {
+        await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+    });
+};
+
+const anchorHandler = (account: string, anchor: HTMLAnchorElement): void => {
+    anchor.addEventListener("click", async () => {
+        const originalText: string = anchor.textContent!;
+
+        try {
+            const cBlockNum: number = await fetchLatestBlock(account);
+            anchor.textContent = `Block: ${cBlockNum}`;
+
+            resetAnchorText(anchor, originalText);
+        } catch (err: any) {
+            console.error("An error has occured while fetching current block");
+        }
+    });
+};
+
+const resetAnchorText = (
+    anchor: HTMLAnchorElement,
+    originalText: string
+): void => {
+    const RESET_TEXT_DELAY = 4000;
+    setTimeout(() => {
+        anchor.textContent = originalText;
+    }, RESET_TEXT_DELAY);
+};
+
+export const navbarState = async (
+    ul: HTMLUListElement,
+    accounts: Array<string>
+): Promise<void> => {
+    while (ul.firstChild) {
+        ul.firstChild.remove();
+    }
+    const account: string | null = accounts[0] || null;
+    if (!account) {
+        // Connect Button
+        const li: HTMLLIElement = document.createElement("li");
+        const a: HTMLAnchorElement = document.createElement("a");
+        a.href = "#";
+        a.textContent = "Connect";
+        li.appendChild(a);
+
+        connectEvent(li);
+        ul.appendChild(li);
+    } else {
+        // Selected Address Element
+        const li1: HTMLLIElement = document.createElement("li");
+        li1.textContent = account;
+
+        // Network Name Element
+        const li2: HTMLLIElement = document.createElement("li");
+        li2.textContent = localStorage.getItem("blockioNetwork");
+
+        // Current Block Anchor
+        const li3: HTMLLIElement = document.createElement("li");
+        const a1: HTMLAnchorElement = document.createElement("a");
+        a1.href = "#";
+        a1.textContent = "What's the latest block?";
+
+        li3.appendChild(a1);
+
+        anchorHandler(account, a1);
+        ul.append(li1, li2, li3);
+    }
+};
---
+++ src/ts/utils/web3/storeNetwork.ts
@@ -0,0 +1,17 @@
+import EthClient from "../../classes/EthClient/EthClient";
+import { Network } from "../../classes/EthClient/types/chains";
+
+export const storeNetwork = async (account: string = "none"): Promise<void> => {
+    async function getChainID(): Promise<number> {
+        const chainID: number = await window.ethereum.request({
+            method: "net_version",
+        });
+        return chainID;
+    }
+
+    const agent: EthClient = new EthClient(window.ethereum, account);
+    const networkData: Network = await agent.network(await getChainID());
+
+    localStorage.setItem("blockioNetwork", networkData.name);
+    localStorage.setItem("blockioCurrency", networkData.currency);
+};
